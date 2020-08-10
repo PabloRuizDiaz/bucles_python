6f92142e0542c2481f67da1607577af513fd40b9
PabloRuizDiaz <Pablo32784964>
2020-08-09 22:02:08 -0300

    Ejercicios Clase 4
    Se resolvieron los ejercicios de la clase 4 sobre bucles

--- ejercicios_clase.py
+++ ejercicios_clase.py
@@ -11,8 +11,8 @@ Programa creado para poner a prueba los conocimientos
 adquiridos durante la clase
 '''
 
-__author__ = "Inove Coding School"
-__email__ = "alumnos@inove.com.ar"
+__author__ = "Pablo Ruiz Diaz"
+__email__ = "rd.pablo@gmail.com"
 __version__ = "1.2"
 
 condicion = False
@@ -20,25 +20,38 @@ condicion = False
 
 def ej1():
     # Ejercicios con bucles "while"
-
-    x = 0
+    
     # Dado el siguiente "while", complete la condicion
     # para que el "while" itere siempre que <x sea menor a 6>
     # Además, complete la línea de código necesaria para que
     # el valor de "x" incremente "1" en cada iteración
 
-    while condicion:    # reemplace "condicion" por lo que crea necesario
-        print("Valor de x =", x)
-        # Coloque la línea de código para que "X" incremente "1"
+    x = 0
+    
+    print('Ejercicio 1 - 1er WHILE')
 
-    x = 5
+    while not condicion:     # reemplace "condicion" por lo que crea necesario
+        print("Valor de x =", x)
+        if x < 6:
+            x += 1           # Coloque la línea de código para que "X" incremente "1"
+            continue
+        break
+        
     # Dado el siguiente "while", complete la condicion
     # para que el "while" itere siempre que <x sea mayor o igual a 0>
     # Además, complete la línea de código necesaria para que
     # el valor de "x" decremente "1" en cada iteración
 
-    while condicion:    # reemplace "condicion" por lo que crea necesario
+    x = 5
+
+    print('\nEjercicio 1 - 2do WHILE')
+
+    while not condicion:    # reemplace "condicion" por lo que crea necesario
         print("Valor de x =", x)
+        if x >= 0:
+            x -= 1
+            continue
+        break
         # Coloque la línea de código para que "X" decremente "1"
 
 
@@ -53,10 +66,20 @@ def ej2():
     # y utilizar como elemento del "for" cada color
     # for color ...
 
+    print('\nEjercicio 2 - 1er FOR')
+
+    for color in colores:
+        print(color)
+
     # Itere el "for" utilizando el tamaño de la lista
     # como parámetro y utilizar el índice para acceder a
     # los elementos de la lista
     # for i ...
+
+    print('\nEjercicio 2 - 2do FOR')
+
+    for i in range(len(colores)):
+        print(colores[i])
 
 
 def ej3():
@@ -67,6 +90,13 @@ def ej3():
     # La sumatoria se deberá ir guardando en la variable "suma"
     numeros = [1, 5, -1, 6, 10, 2, -5]
     suma = 0   # Variable ya inicializada, la suma arranca en cero
+
+    print('\nEjercicio 3')
+
+    for i in range(len(numeros)):
+        suma += numeros[i]
+
+    print('La suma de todos los valores de la lista "numeros" es {}' .format(suma))
 
 
 def ej4():
@@ -80,12 +110,28 @@ def ej4():
     # En cada iteracion del bucle debe incrementar el valor de "x" en "2"
     # e imprimir en pantalla el resultado de X (antes de incrementar) con print
 
+    print('\nEjercicio 4 - A)')
+
+    while (x < 10 and x != 6):
+        print('El valor de x es ', x)
+        x += 2
+
     # Realice el mismo bucle "while" pero en vez de estar formado por una condición
     # compuesta, que el "while" siga iterando mientras <x sea menos a 10>, y dentro del
     # "while" consultar si <x es igual a 6>, y en ese caso realizar una interrupción del bucle
     # En cada iteracion del bucle debe incrementar el valor de "x" en "2"
     # e imprimir en pantalla el resultado de X (antes de incrementar) con print
 
+    print('\nEjercicio 4 - B)')
+
+    x = 0
+
+    while x < 10:
+        if x == 6:
+            break
+    
+        print('El valor de x es', x)
+        x += 2
 
 def ej5():
     # Ejercicio de secuencias numéricas
@@ -97,11 +143,14 @@ def ej5():
     # sino que va hasta el anterior
 
     inicio = int(input('Ingrese el primero número de la secuencia\n'))
-    # fin....
+    fin = int(input('Ingrese el ultimo número de la secuencia\n'))
+    
+    suma = 0
 
-    # for ... in range(....)
+    for i in range(inicio,fin+1):
+        suma += i
 
-    # Imprimir el valor de la sumatoria
+    print('La sumatoria es', suma)
 
 
 def ej6():
@@ -112,23 +161,31 @@ def ej6():
     # y cuante cuantes números son negativos y cuantos números son mayor o igual a cero
     # Tener en cuenta que "range" no incluye el número de "fin" en su secuencia,
     # sino que va hasta el anterior
-
-    inicio = int(input('Ingrese el primero número de la secuencia\n'))
-    # fin....
-
-    cantidad_numeros_positivos = 0  # Inicializo el contador en 0
-    #cantidad_numeros_negativos
-
-    # for ... in range(....)
-
     # Imprimir el valor de la cantidad de números positivos y negativos
-
+    
+    print('\nEjercicio 6')
+    
+    inicio = int(input('Ingrese el primero número de la secuencia\n'))
+    fin = int(input('Ingrese el ultimo número de la secuencia\n'))
+    
+    cant_num_pos = 0
+    cant_num_neg = 0
+    
+    listas = range(inicio, fin + 1)
+    
+    for i in listas:
+        if listas[i] < 0:
+            cant_num_neg += 1
+        else:
+            cant_num_pos += 1
+    
+    print('Cantidad de numeros negativos {} y positivos {}' .format(cant_num_neg, cant_num_pos))
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
     ej1()
-    #ej2()
-    #ej3()
-    #ej4()
-    #ej5()
-    #ej6()
+    ej2()
+    ej3()
+    ej4()
+    ej5()
+    ej6()
--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -11,8 +11,8 @@ Programa creado para que practiquen los conocimietos
 adquiridos durante la semana
 '''
 
-__author__ = "Inove Coding School"
-__email__ = "alumnos@inove.com.ar"
+__author__ = "Pablo Ruiz Diaz"
+__email__ = "rd.pablo@gmail.com"
 __version__ = "1.1"
 
 # Variable global utilizada para el ejercicio de nota
@@ -24,7 +24,7 @@ temp_dataloger = [12.8, 18.6, 14.5, 20.8, 12.1, 21.2, 13.5, 18.6,
 
 
 def ej1():
-    print('Comenzamos a ponernos serios!')
+    print('Comenzamos a ponernos serios!\n')
 
     '''
     Realice un programa que pida por consola dos números que representen
@@ -35,22 +35,22 @@ def ej1():
     sino que va hasta el anterior
     '''
 
-    # inicio = ....
-    # fin = ....
+    cuenta = 0
+    sumatoria = 0
+    
+    inicio = int(input('Ingrese 1er numero de la secuencia:\n'))
+    fin = int(input('Ingrese ultimo numero de la secuencia:\n'))
 
-    # cantidad_numeros ....
-    # sumatoria ....
+    numeros = range(inicio, fin + 1)
 
-    # bucle.....
+    for i in range(len(numeros)):
+        cuenta += 1
+        sumatoria += numeros[i]
 
-    # Al terminar el bucle calcular el promedio como:
-    # promedio = sumatoria / cantidad_numeros
-
-    # Imprimir resultado en pantalla
-
+    print('\nCantidad de numeros en la lista {}, y su sumatoria es {}' .format(cuenta, sumatoria))
 
 def ej2():
-    print("Mi Calculadora (^_^)")
+    print("\nMi Calculadora (^_^)")
 
     '''
     Tome el ejercicio de clase:
@@ -62,10 +62,30 @@ def ej2():
     Se debe debe imprimir un cartel de error si el operador ingresado no es
     alguno de lo soportados o no es la palabra "FIN"
     '''
+    
+    while True:
+        numero_1 = float(input('Ingrese el primer número:\n'))
+        numero_2 = float(input('Ingrese el segundo número:\n'))
+        operacion = str(input('Ingrese la operacion a ejecutar:\n + -> suma \n - -> resta \n * -> multiplicacion \n / -> division \n ** -> potenciacion \n Fin -> Finaliza operacion \n'))
+
+        if operacion == 'Fin':
+            break
+        elif operacion == '+':
+            print('La suma es {}' .format(numero_1 + numero_2))
+        elif operacion == '-':
+            print('La resta es {}' .format(numero_1 - numero_2))
+        elif operacion == '*':
+            print('La multiplicacion es {}' .format(numero_1 * numero_2))
+        elif operacion == '/':
+            print('La division es {}' .format(numero_1 / numero_2))
+        elif operacion == '**':
+            print('{} a la potencia de {} es {}' .format(numero_1, numero_2, numero_1 ** numero_2))
+        else:
+            print('ERROR: Operacion matematica no existente.')
 
 
 def ej3():
-    print("Mi organizador académico (#_#)")
+    print("\nMi organizador académico (#_#)")
 
     '''
     Tome el ejercicio de "calificaciones":
@@ -76,7 +96,7 @@ def ej3():
     Las notas del estudinte se encuentran almacenadas en una
     lista llamada "notas" que ya hemos definido al comienzo del archivo
 
-    Debe caluclar el promedio de todas las notas y luego transformar
+    Debe calcular el promedio de todas las notas y luego transformar
     la califiación en una letra según la escala establecida en el ejercicio
     "calificaciones" <condicionales_python / ejercicios_clase / ej3>
 
@@ -86,34 +106,40 @@ def ej3():
     Debe contar la cantidad de notas válidas y la cantidad de ausentes
     '''
 
-    # Para calcular el promedio primero debe obtener la suma
-    # de todas las notas, que irá almacenando en esta variable
-    sumatoria = 0           # Ya le hemos inicializado en 0
+    sumatoria = 0
+    cantidad_notas = 0 
+    cantidad_ausentes = 0
 
-    cantidad_notas = 0      # Aquí debe contar cuantas notas válidas encontró
-    cantidad_ausentes = 0   # Aquí debe contar cuantos ausentes hubo
+    for nota in notas:
+        if nota >= 0:
+            sumatoria += nota
+            cantidad_notas += 1
+        else:
+            cantidad_ausentes += 1
 
-    # Realice aquí el bucle para recorrer todas las notas
-    # y cacular la sumatoria
+    puntaje = sumatoria / cantidad_notas   #puntaje promedio de notas
 
-    # Terminado el bucle calcule el promedio como
-    # promedio = sumatoria / cantidad_notas
+    print('El alumno tiene {} examenes rendidos y {} ausentes' .format(cantidad_notas, cantidad_ausentes))
+    print('La nota final promedio es:')
 
-    # Utilice la nota promedio calculada y transformela
-    # a calificación con letras, imprima en pantalla el resultado
-
-    # Imprima en pantalla al cantidad de ausentes
-
+    if puntaje >= 60:
+        if puntaje >= 70:
+            if puntaje >= 80:
+                if puntaje >= 90:
+                    print('A')
+                else:
+                    print('B')
+            else:
+                print('C')
+        else:
+            print('D')
+    else:
+        print('F')
 
 def ej4():
-    print("Mi primer pasito en data analytics")
+    print("\nMi primer pasito en data analytics")
 
     '''
-    Tome el ejercicio:
-    <condicionales_python / ejercicios_practica /ej5>,
-    copielo a este ejercicio y modifíquelo para cumplir el
-    siguiente requerimiento
-
     En este ejercicio se lo provee de una lista de temperatuas,
     esa lista de temperatuas corresponde a los valores de temperaturas
     tomados durante una temperorada del año en Buenos Aires.
@@ -141,10 +167,21 @@ def ej4():
     temperatura_min = None      # Aquí debe ir almacenando la temp mínima
     temperatura_sumatoria = 0   # Aquí debe ir almacenando la suma de todas las temp
     temperatura_promedio = 0    # Al finalizar el loop deberá aquí alamcenar el promedio
-    temperatura_len = 0         # Aquí debe almacenar cuantas temperatuas hay en la lista
+    #temperatura_len = 0         # Aquí debe almacenar cuantas temperatuas hay en la lista
 
-    # Colocar el bucle aqui......
+    for x in temp_dataloger:
+        if (temperatura_max is None or temperatura_max < x):
+            temperatura_max = x
+        
+        if (temperatura_min is None or temperatura_min > x):
+            temperatura_min = x
+        
+        temperatura_sumatoria += x
+    
+    temperatura_promedio = temperatura_sumatoria / len(temp_dataloger)
 
+    print('\nTemperatura max {} y min {}. Con un promedio de {}' .format(temperatura_max, temperatura_min, temperatura_promedio))
+ 
     # Al finalizar el bucle compare si el valor que usted calculó para
     # temperatura_max y temperatura_min coincide con el que podría calcular
     # usando la función "max" y la función "min" de python
@@ -157,6 +194,14 @@ def ej4():
     # Corroboren los resultados de temperatura_sumatoria
     # usando la función "sum"
     # función "sum" --> https://www.w3schools.com/python/ref_func_sum.asp
+
+    print('\nPor Python')
+
+    temperatura_max = max(temp_dataloger)
+    temperatura_min = min(temp_dataloger)
+    temperatura_promedio = sum(temp_dataloger) / len(temp_dataloger)
+
+    print('\nTemperatura max {} y min {}. Con un promedio de {}' .format(temperatura_max, temperatura_min, temperatura_promedio))
 
     '''
     Una vez que tengamos nuestros valores correctamente calculados debemos
@@ -173,14 +218,27 @@ def ej4():
     https://es.weatherspark.com/y/28981/Clima-promedio-en-Buenos-Aires-Argentina-durante-todo-el-a%C3%B1o
     '''
 
-    # En base a los rangos de temperatura de cada estación,
-    # ¿En qué época del año nos encontramos?
-    # Imprima el resultado en pantalla
-    # Debe utilizar temperatura_max y temperatura_min para definirlo
+    verano = [19,28]
+    otono = [11,24]
+    invierno = [8,14]
+    primavera = [10,24]
 
+    #Las estaciones se organizaron en referencia de menor a mayor en temperaturas minimas,
+    #y comparando si el rango esta dentro de cada rango promedio por estacion
+    #Esta analogia tiene el problema que otono y primavera son parecidos,
+    #por lo que deberia de juntarse algun dato mas como humedad para ser mas precisos.
+
+    if temperatura_min >= min(invierno) and temperatura_max <= max(invierno):
+        print('Invierno')
+    elif temperatura_min >= min(primavera) and temperatura_max <= max(primavera):
+        print('Primavera')
+    elif temperatura_min >= min(otono) and temperatura_max <= max(otono):
+        print('Otono')
+    else:
+        print('Verano')
 
 def ej5():
-    print("Ahora sí! buena suerte :)")
+    print("\nAhora sí! buena suerte :)")
 
     '''
     Tome el ejercicio:
@@ -246,11 +304,57 @@ def ej5():
 
   '''
 
+    while True:
+        i = 0
+        palabras = []
+        palabras_max = []
+        palabras_largo_max = []
 
+        operacion_txt = str(input('Ingrese accion a realizar: \n 1 -> Orden alfabetico \n 2 -> Cantidad de letras \n 3 -> Finalizar \n'))
+
+        if operacion_txt == '3':
+            break
+        elif operacion_txt != '1' and operacion_txt != '2':
+            print('ERROR: Operacion no valida.\n')
+            continue
+        
+        while True:
+            palabra = str(input('Ingrese palabra (para finalizar escriba *):\n'))
+            
+            if palabra == '*':
+                break
+            
+            palabras.append(palabra)
+            
+        copy_palabras = palabras
+
+        if operacion_txt == '1':
+            for i in range(len(palabras)):
+                palabra_max = None
+                for palabra in copy_palabras:
+                    if (palabra_max is None or palabra_max < palabra):
+                        palabra_max = palabra
+                palabras_max.append(palabra_max)
+                copy_palabras.remove(palabra_max)
+            print('Orden alfabetico\n {}' .format(palabras_max))
+        elif operacion_txt == '2':
+            for i in range(len(palabras)):
+                palabra_max = None
+                largo_max = None
+                for palabra in copy_palabras:
+                    if (largo_max is None or largo_max < len(palabra)):
+                        largo_max = len(palabra)
+                        palabra_max = palabra
+                palabras_largo_max.append(largo_max)
+                copy_palabras.remove(palabra_max)
+            print('Orden de largo\n {}' .format(palabras_largo_max))
+
+    print('Finalizado')
+ 
 if __name__ == '__main__':
     print("Ejercicios de práctica")
-    #ej1()
-    #ej2()
-    #ej3()
-    #ej4()
-    #ej5()
+    ej1()
+    ej2()
+    ej3()
+    ej4()
+    ej5()
